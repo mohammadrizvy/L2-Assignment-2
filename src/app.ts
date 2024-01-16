4323b0092e4673e74605bd2e50ec1c27d295f4ce
MD Ehsanul Haque Rizvy <rizvyhq1@gmail.com>
2024-01-17 02:31:32 +0600

    ts-node-dev & prettier installed

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
-import express, { Application, Request, Response } from "express";
-import cors from 'cors'
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
-const app : Application = express();
+const app: Application = express();
 
-// Using parser from exprsss ; 
+// Using parser from exprsss ;
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('The server of Assignmet 2 is RUNNING');
 });
-
 
 // console.log(process.cwd());
 
-export default app ; 
+export default app;
