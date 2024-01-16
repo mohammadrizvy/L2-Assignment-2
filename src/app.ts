82291e55ab69b98164e7174ac3dfbfeb389cab5e
MD Ehsanul Haque Rizvy <rizvyhq1@gmail.com>
2024-01-17 01:21:56 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from "express";
+import cors from 'cors'
+
+const app : Application = express();
+
+// Using parser from exprsss ; 
+
+app.use(express.json());
+app.use(cors())
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+// console.log(process.cwd());
+
+export default app ; 
