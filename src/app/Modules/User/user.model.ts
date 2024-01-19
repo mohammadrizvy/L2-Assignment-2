572fc8987e8a22ed4b1d058f9035310706e8f0dd
MD Ehsanul Haque Rizvy <rizvyhq1@gmail.com>
2024-01-20 04:46:09 +0600

    Creating model complete

--- src/app/Modules/User/user.model.ts
+++ src/app/Modules/User/user.model.ts
@@ -1,28 +1,40 @@
 import { Schema, model, connect } from 'mongoose';
-import { User } from './user.interface';
+import { Address, FullName, Orders, User } from './user.interface';
+
+const fullNameSchema = new Schema<FullName>({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+});
+
+const addressSchema = new Schema<Address>({
+  street: { type: String },
+  city: { type: String },
+  country: { type: String },
+});
+
+const ordersSchema = new Schema<Orders>([
+  {
+    productName: { type: String },
+    price: { type: Number },
+    quantity: { type: Number },
+  },
+]);
 
 const userSchema = new Schema<User>({
   userId: { type: Number },
   username: { type: String },
   password: { type: String },
-  fullName: {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-  },
+
+  fullName: fullNameSchema,
+
   age: { type: Number },
   email: { type: String },
   isActive: { type: Boolean },
   hobbies: { type: [String], required: true },
-  address: {
-    street: { type: String },
-    city: { type: String },
-    country: { type: String },
-  },
-  orders: [
-    {
-      productName: { type: String },
-      price: { type: Number },
-      quantity: { type: Number },
-    },
-  ],
+  address: addressSchema,
+
+  orders: ordersSchema,
 });
+
+
+const User = model<User>('User', userSchema);
